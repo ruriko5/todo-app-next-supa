b637650808dd2335ddd46fc2d7c25f63d1f4a33f
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-02-09 02:03:49 +0900

    signout

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
+import { signout } from "../login/actions";
 
 export default async function PrivatePage() {
   const supabase = await createClient();
@@ -10,5 +11,12 @@ export default async function PrivatePage() {
     redirect("/login");
   }
 
-  return <p>Hello {data.user.email}</p>;
+  return (
+    <div>
+      <p>Hello {data.user.email}</p>
+      <form>
+        <button formAction={signout}>Sign out</button>
+      </form>
+    </div>
+  );
 }
