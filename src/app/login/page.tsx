cd60ed569a24f0aee0c1448ece9145ed2e87b363
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-02-10 02:35:20 +0900

    Login Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,61 @@
 import { login, signup } from "./actions";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default async function LoginPage() {
   const supabase = await createClient();
   const { data } = await supabase.auth.getUser();
   if (data.user) return redirect("/private");
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex justify-center items-center flex-col h-screen">
+      <Card className="">
+        <CardHeader className="space-y-4">
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div>
+              <Label htmlFor="email">Email:</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                required
+                placeholder="email@example.com"
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">Password:</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                required
+                minLength={8}
+              />
+            </div>
+            <Button formAction={login} className="block w-full">
+              Log in
+            </Button>
+            <Button formAction={signup} className="block w-full">
+              Sign up
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
