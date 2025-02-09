b4d813e489b44e7e4d75b8f7a309915467856d0f
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-02-10 01:23:46 +0900

    if is_login redirect /private

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,11 @@
 import { login, signup } from "./actions";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getUser();
+  if (data.user) return redirect("/private");
   return (
     <form>
       <label htmlFor="email">Email:</label>
